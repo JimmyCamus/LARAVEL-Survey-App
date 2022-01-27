7fde2c8774f9b019fe6f8db93f8c13a477f68102
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-01-27 14:35:35 -0300

    initial setup.

---
+++ resources/js/components/Layout.js
@@ -0,0 +1,12 @@
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <p>Layout Component Navbar</p>
+      {children}
+      <p>Layout Component Footer</p>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ resources/js/index.js
@@ -0,0 +1,16 @@
+import Layout from "./components/Layout";
+
+const Index = () => {
+  return (
+    <div>
+      <Layout>
+        <div>
+          <p>Component 1</p>
+          <p>Component 2</p>
+        </div>
+      </Layout>
+    </div>
+  );
+};
+
+export default Index;
